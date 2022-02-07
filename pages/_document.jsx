6184588d0627f07296bf9496a7a6b46984512ef9
marimarquesim <mari.marquesim@gmail.com>
2022-02-07 17:07:00 -0300

    ceo fix

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 import { GA_TRACKING_ID } from '../lib/gtag'
 
