49a4c2241fb6e8f46e6d229741e835ed6677b3e4
marimarquesim <mari.marquesim@gmail.com>
2022-02-07 17:04:46 -0300

    tag ceo

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next'
 
 import { GA_TRACKING_ID } from '../lib/gtag'
 
@@ -16,12 +16,34 @@ class WebDocument extends Document {
         gtag('js', new Date()); 
          gtag('config', '${GA_TRACKING_ID}');`}} />
           {/* <!-- End Global site tag (gtag.js) - Google Ads: 860794148 --> */}
+          <script dangerouslySetInnerHTML={{
+                    __html: `!function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '1082080379002942');
+                    fbq('track', 'PageView');`}} />
+          
 
                 </Head>
                 
                 <body>
                     <Main />
                     <NextScript/>
+                    <noscript dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXX"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+                
+                <noscript dangerouslySetInnerHTML={{
+                    __html: `<img height="1" width="1" style="display:none"
+                    src="https://www.facebook.com/tr?id=1082080379002942&ev=PageView&noscript=1"
+                  />`
+                }}></noscript>
+                
                 </body>
 
             </Html>
